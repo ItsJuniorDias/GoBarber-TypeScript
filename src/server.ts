52e162777b4e05dd016cb84f6f332843e2a6e97d
ItsJuniorDias <juniordias_@live.com>
2020-07-26 20:36:13 -0300

    chore: Initial Structure

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello GoBarber'});
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
