9681131ca25504c210ccb6ba7b8cfca011032105
ItsJuniorDias <juniordias_@live.com>
2020-07-27 20:34:29 -0300

    chore: Create Router appointment

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoBarber' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
 });
+
