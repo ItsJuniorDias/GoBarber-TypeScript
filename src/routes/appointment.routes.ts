82329ee62a7c896d5147eb1327094bb81ce7d09a
ItsJuniorDias <juniordias_@live.com>
2020-07-27 21:26:24 -0300

    chore: Appointment

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -1,5 +1,12 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  return response.json({ message: 'Hello World '});
+})
 
 export default appointmentsRouter;
