8a559b96c89bebcc74e4b4f589462ab0429c5c93
ItsJuniorDias <juniordias_@live.com>
2020-08-01 20:46:17 -0300

    chore: Create model

--- backend/src/routes/appointment.routes.ts
+++ backend/src/routes/appointment.routes.ts
@@ -1,27 +1,29 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date),
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
   );
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This appoinment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
   return response.json(appointment);
-})
+});
 
 export default appointmentsRouter;
