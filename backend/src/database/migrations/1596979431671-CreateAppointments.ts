da087af585e101a90af1adb03c2b2c8b88191de1
ItsJuniorDias <juniordias_@live.com>
2020-08-16 21:34:36 -0300

    chore: Create Appointment

--- backend/src/database/migrations/1596979431671-CreateAppointments.ts
+++ backend/src/database/migrations/1596979431671-CreateAppointments.ts
@@ -12,6 +12,7 @@ export class CreateAppointments1596979431671 implements MigrationInterface {
                  type: 'varchar',
                  isPrimary: true,
                  generationStrategy: 'uuid',
+                 default: 'uuid_generate_v4()',
                },
 
                {
