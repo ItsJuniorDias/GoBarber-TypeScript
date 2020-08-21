7209f69c52d8ed86d6a572f43206304707260565
ItsJuniorDias <juniordias_@live.com>
2020-08-20 21:40:22 -0300

    chore: Decorators functions

--- exemple/src/database/migrations/1597968472017-CreateAppointments.ts
+++ exemple/src/database/migrations/1597968472017-CreateAppointments.ts
@@ -1,11 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateAppointments1597968472017 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+               name: 'appointments',
+               columns: [
+                {
+                    name: 'id',
+                    type: 'varchar',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                },
+                {
+                    name: 'provider',
+                    type: 'varchar',
+                    isNullable: false,
+                },
+                {
+                    name: 'date',
+                    type: 'timestamp with time zone',
+                    isNullable: false,
+                }
+               ]
+            })
+        );
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
+    public async down(queryRunner: QueryRunner): Promise<void> {
+       await queryRunner.dropTable('appointments');
     }
 
 }
--- exemple/src/models/Appointments.ts
+++ exemple/src/models/Appointments.ts
@@ -1,21 +1,14 @@
-import { uuid } from 'uuidv4';
-
-interface AppointmentConstructor {
-  provider: string;
-  date: Date;
-}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'; 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  
+  @Column()
   provider: string;
-
+  
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
