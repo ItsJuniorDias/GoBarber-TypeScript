3799c78dde78341c751a88696eb757f5a3ea1078
ItsJuniorDias <juniordias_@live.com>
2020-08-20 21:09:03 -0300

    chore: migrations

---
+++ exemple/src/database/migrations/1597968472017-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1597968472017 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
