af626fd3e408ec1c010aa36bebb3441cd5fe4d56
Junior Dias <juniordias_@live.com>
2020-08-22 18:33:37 -0300

    chore: Config Typeorm

--- backend/src/repositories/AppointmentsRepository.ts
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -1,37 +1,19 @@
-import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointments';
+import { EntityRepository, Repository} from 'typeorm';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment>{
+  public async findByDate(date: Date): Promise<Appointment | null> {
 
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = []
-  }
-
-  public all(): Appointment[] {
-    return this.appointments; 
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment => 
-      isEqual(date, appointment.date),   
-    );
+    const findAppointment =  await this.findOne({
+      where: {
+        date
+      }
+    })
 
     return findAppointment || null;
   } 
 
-  public create({date, provider}:CreateAppointmentDTO ): Appointment {
-    const appointment = new Appointment({ provider, date });
-
-    this.appointments.push(appointment);
-
-    return appointment;
-  } 
 }
 
 export default AppointmentsRepository;
