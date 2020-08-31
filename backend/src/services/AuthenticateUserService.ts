a6bcbcc8803caf8140d0a0fabdc092e3911e33d8
Junior Dias <juniordias_@live.com>
2020-08-31 20:31:51 -0300

    chore: Token Jwt

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string
 }
 
 class AuthenticateUserService {
@@ -27,8 +29,14 @@ class AuthenticateUserService {
        throw new Error('Incorrect email/password combination');
      }
 
+     const token = sign({ }, 'cb4f0d246dc8156e6c20d755123c4cd8', {
+       subject: user.id,
+       expiresIn: '1d',
+     });
+
      return {
        user,
+       token,
      };
   }
 }
