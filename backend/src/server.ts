dd57f4cc2654e883ef2866001467b28069ddb71b
Junior Dias <juniordias_@live.com>
2020-09-12 21:21:21 -0300

    chore: cors add

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction} from 'express';
 import 'express-async-errors';
 import routes from './routes';
+import cors from 'cors';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
