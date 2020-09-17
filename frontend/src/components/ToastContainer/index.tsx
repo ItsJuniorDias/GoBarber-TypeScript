53b41d3a09ce05faabee7a574c54b565e03cd53e
Junior Dias <juniordias_@live.com>
2020-09-16 21:55:23 -0300

    chore: Add and Remove toast

--- frontend/src/components/ToastContainer/index.tsx
+++ frontend/src/components/ToastContainer/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { ToastMessage } from '../../hooks/toast';
-import { Container, Toast } from './styles';
+
+import Toast from './Toast';
+import { ToastMessage, useToast } from '../../hooks/toast';
+import { Container } from './styles';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
@@ -11,22 +12,7 @@ const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
     <Container>
       {messages.map((message) => (
-        <Toast
-          key={message.id}
-          type={message.type}
-          hasDescription={!!message.description}
-        >
-          <FiAlertCircle size={20} />
-
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button type="button">
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
