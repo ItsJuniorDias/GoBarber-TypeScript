2fe7d10f2739e2ccc4adaa85f4fb4c0f1d142d68
Junior Dias <juniordias_@live.com>
2020-09-28 09:18:08 -0300

    chore: Update Files, OK

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -5,7 +5,7 @@ import { View, StatusBar } from 'react-native';
 
 const App: React.FC = () => (
   <>
-    <StatusBar barStyle="light-content" backgroundColor="312e38" />
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <View style={{ flex: 1, backgroundColor: '#312e38' }} />
   </>
 );
