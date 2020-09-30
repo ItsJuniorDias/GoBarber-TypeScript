ef4557277cf3a65fed2dfe3ba32f00768924e57f
Junior Dias <juniordias_@live.com>
2020-09-29 21:22:41 -0300

    chore: React Navigation Stack

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
 
-import { Container } from './styles';
+// import { Container } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return <View />;
 };
 
 export default SignUp;
