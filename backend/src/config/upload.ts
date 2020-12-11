3cc8660bf1d6499932bcaa46e1249115b8483b13
Alexandre Junior <juniordias_@live.com>
2020-12-11 18:37:12 -0300

    chore: Review

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -13,6 +13,6 @@ export default {
       const fileName = `${fileHash}-${file.originalname}`;
 
       return callback(null, fileName);
-    }
+    },
   }),
-}
+};
--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -11,29 +11,29 @@ interface TokenPayload{
 }
 
 export default function ensureAuthenticated(
-   request: Request, 
-   response: Response, 
-   next: NextFunction
-  ): void {
-   const authHeader = request.headers.authorization;
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
 
-   if(!authHeader) {
-     throw new AppError('JWT token is missing', 401);
-   }
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
 
-   const [, token] = authHeader.split(' ');
+  const [, token] = authHeader.split(' ');
 
-   try {
-    const decoded = verify(token, authConfig.jwt.secret); 
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
 
     const { sub } = decoded as TokenPayload;
 
     request.user = {
       id: sub,
-    }
+    };
 
     return next();
-   } catch(err) {
-     throw new Error('Invalid JWT Token');
-   }
-} 
+  } catch (err) {
+    throw new Error('Invalid JWT Token');
+  }
+}
