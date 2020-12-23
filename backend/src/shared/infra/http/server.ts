68215faf79190ef43c599783f4d8aa1a74f4e779
Alexandre Junior <juniordias_@live.com>
2020-12-23 14:30:02 -0300

    chore: Appointment Inject depency

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -2,14 +2,15 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import routes from './routes';
 import cors from 'cors';
 
 import uploadConfig from '@config/upload';
 
 import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
@@ -18,21 +19,23 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err:Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message,
+      message: 'Internal server error',
     });
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server Started on port 3333! ');
