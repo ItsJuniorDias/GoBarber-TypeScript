b641efcd8ca09aabc4de8b3dd583dd1b288b47af
Alexandre Junior <juniordias_@live.com>
2020-12-23 13:49:19 -0300

    chore: Implement Methods User

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -1,17 +1,16 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 
-import AppError from '@shared/errors/AppError';
-
-interface Request {
+interface IRequest {
   name: string;
   email: string;
   password: string;
 }
 
 class CreateUserService {
-  public async execute({ name, email, password }: Request): Promise<User> {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
     const usersRepository = getRepository(User);
 
     const checkUserExists = await usersRepository.findOne({
--- backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,17 +4,17 @@ import fs from 'fs';
 
 import uploadConfig from '@config/upload';
 
-import User from '../infra/typeorm/entities/User';
-
 import AppError from '@shared/errors/AppError';
 
-interface Request {
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
   user_id: string;
   avatarFilename: string;
 }
 
 class UpdateUserAvatarService {
-  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne(user_id);
