965f4de02e5dd3bd5f80487c035ded2feb3ccd9e
Alexandre Junior <juniordias_@live.com>
2021-01-03 13:54:38 -0300

    chore: Methods Save and Delete UpdateUser

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -5,7 +5,9 @@ import multer from 'multer';
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  tmpFolder,
+
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
