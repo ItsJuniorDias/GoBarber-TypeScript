e2c5d1932e263bb5c9cebe9c42518b4f89fa8e95
Alexandre Junior <juniordias_@live.com>
2021-01-05 09:43:27 -0300

    chore: Implements Ethereal Mail

---
+++ backend/test.js
@@ -0,0 +1,42 @@
+// Faça endpoints para CRUD de VEICULOS por CONTAS.
+
+// HTTP CODE PARA RESPONSE
+// HTTP CODE PARA REQUEST
+// HTTP PATH PARA ENDPOINTS
+
+
+Model Veiculo
+ id
+ name
+ color
+ type
+
+- POST
+- Request
+- Reponse return veiculo
+
+Get
+- Response
+ -
+
+ - Delete  request
+ - /car/:id/
+
+ - UPDATE
+  - request
+
+  GET - /car
+  PAYLOAD(name, color, type)
+  RESPONSE 200 (OK)
+
+  DELETE /car/id
+  PAYLOAD(ID)
+  RESPONSE - 200 (OK)
+
+
+
+
+
+
+
+
