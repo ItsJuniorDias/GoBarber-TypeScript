2b5c1ae4eb31f0bc3c92093ea0f3574b5eb6d50c
Alexandre Junior <juniordias_@live.com>
2021-01-05 15:32:53 -0300

    chore: DTO Mail Template

--- backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,5 +1,6 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
@@ -22,12 +23,18 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({ to, from, subject }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipe@gobarber.com>',
-      to,
-      subject: 'Recuperação de senha',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      text: 'Test',
     });
 
     console.log('Message sent: %s', message.messageId);
