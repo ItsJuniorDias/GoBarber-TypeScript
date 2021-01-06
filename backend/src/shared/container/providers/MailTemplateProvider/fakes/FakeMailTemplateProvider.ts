45583488fa0a400879497cc5ea310cabf59f10f8
Alexandre Junior <juniordias_@live.com>
2021-01-06 10:04:27 -0300

    chore: Template hbs forgot password

--- backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
-import IMailProvider from '../models/IMailTemplateProvider';
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
-class FakeMailTemplateProvider implements IMailProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+class FakeMailTemplateProvider implements IMailTemplateProvider {
+  public async parse(): Promise<string> {
+    return 'Mail Content';
   }
 }
 
