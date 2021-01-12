466711a4897590fd896853604ff931ee9330fc85
Alexandre Junior <juniordias_@live.com>
2021-01-12 11:01:25 -0300

    chore: Routes Providers

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -3,12 +3,17 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providersMonthController = new ProviderMonthAvailabilityController();
+const providersDayController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
-
 providersRouter.get('/', providersController.index);
+providersRouter.get('/:id/month-availability', providersMonthController.index);
+providersRouter.get('/:id/day-availability', providersDayController.index);
 
 export default providersRouter;
